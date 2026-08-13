507fcef6821844d2fc0b367f30b8b3145f7b5bbd
Robert van den Boom <vdboom.robert@gmail.com>
2026-08-13 09:22:04 +0200

    feat: current time & additional files

---
+++ js/db.js
@@ -0,0 +1,34 @@
+const users = [
+  {
+    id: 1,
+    name: "User 1",
+    email: "user1@example.com"
+  },
+  {
+    id: 2,
+    name: "User 2",
+    email: "user2@example.com"
+  },
+  {
+    id: 3,
+    name: "User 3",
+    email: "user3@example.com"
+  }
+];
+
+const tasks = [
+  {
+    id: 1,
+    category: "Category 1",
+    title: "Task 1",
+    description: "Description for Task 1",
+    preview : "short Description for Task 1",
+    subtasks: [
+      { id: 1.1, title: "Subtask 1.1", completed: false },
+      { id: 1.2, title: "Subtask 1.2", completed: true },
+    ],
+    collaborators: [
+      { id: 1, name: "Collaborator 1", email: "collaborator1@example.com" }
+    ],
+  }
+];
---
+++ script.js
@@ -0,0 +1,3 @@
+// für Berechnung Deadline + Begrüßung (Guten Morgen, Guten Tag, Guten Abend)
+let currentTime = new Date().toLocaleTimeString();
+console.log("Current Time:", currentTime);
