1474a84fef3e1cd24d87825afeee5f4c23475621
Micha Gut <m.gut@windowslive.com>
2026-08-17 20:34:46 +0200

    feat: add firebase-config.js with app/auth/database init, link as ES modules

---
+++ js/firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDgb6_oZTEO6-NnYFIYtQyuAAef7AszIY0",
+    authDomain: "join-3236.firebaseapp.com",
+    projectId: "join-3236",
+    storageBucket: "join-3236.firebasestorage.app",
+    messagingSenderId: "1075341106587",
+    appId: "1:1075341106587:web:94df0c65166a709f6b953e"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const database = getDatabase(app);
